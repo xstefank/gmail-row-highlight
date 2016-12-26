7208c8e5d19a5afed00567bff2dd047752c407c3
xstefank <mjolnir122@gmail.com>
2016-12-26 23:46:56 +0100

    add main color support; add preserveDefaultColor support

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 
-var tabindexObserver = new MutationSummary({
+var defBgColor = null;
+var defColor = null;
+
+var tabindexObserver = new MutationSummary ({
     callback: tabindexChangeHandler,
     queries: [{ attribute: 'tabindex' }]
 });
@@ -10,9 +13,20 @@ function tabindexChangeHandler(trs) {
         var currentValue = changeEl.getAttribute('tabindex');
 
         if (currentValue == 0) {
+            preserveDefaultColor(changeEl);
             changeEl.style.backgroundColor = "yellow";
+            changeEl.style.color = "blue";
         } else {
-            changeEl.style.backgroundColor = "initial";
+            changeEl.style.backgroundColor = defBgColor;
+            changeEl.style.color = defColor;
         }
     });
 }
+
+function preserveDefaultColor(elem) {
+    if (defBgColor != null) return;
+
+    defBgColor = elem.style.backgroundColor;
+    defColor = elem.style.color;
+}
+
