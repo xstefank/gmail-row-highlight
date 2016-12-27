795262953df9e43084de660b797295258374ccbc
xstefank <mjolnir122@gmail.com>
2016-12-27 22:22:33 +0100

    content.js - add documentation

--- content.js
+++ content.js
@@ -1,10 +1,13 @@
 
+//default colors
 var defBgColor = null;
 var defColor = null;
 
+//user colors
 var userBgColor = "#A0A0A0";
 var userColor = "#E0E0E0";
 
+//row change observer
 var tabindexObserver = new MutationSummary ({
     callback: tabindexChangeHandler,
     queries: [{ attribute: 'tabindex' }]
@@ -16,14 +19,19 @@ function tabindexChangeHandler(trs) {
         var currentValue = changeEl.getAttribute('tabindex');
 
         if (currentValue == 0) {
+            //selected row
             preserveDefaultColor(changeEl);
             setRowColor(changeEl, userBgColor, userColor);
         } else {
+            //deselected row
             setRowColor(changeEl, defBgColor, defColor);
         }
     });
 }
 
+/*
+saves the the default color values so they can be used when the row is deselected
+ */
 function preserveDefaultColor(elem) {
     if (defBgColor != null) return;
 
@@ -31,6 +39,9 @@ function preserveDefaultColor(elem) {
     defColor = elem.style.color;
 }
 
+/*
+set the row colors
+ */
 function setRowColor(elem, bgCol, col) {
     elem.style.backgroundColor = bgCol;
     elem.style.color = col;
