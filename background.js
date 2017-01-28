7ce829d66e61fb3353b50c5d5762b4668b40945f
xstefank <mjolnir122@gmail.com>
2017-01-28 08:34:17 +0100

    switch from background.js to popup.html; remove background.js

--- background.js
+++
@@ -1,4 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-    var newURL = "https://github.com/xstefank/gmail-row-highlight";
-    chrome.tabs.create({ url: newURL });
-});
---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById("test").addEventListener("click", sayHello);
+
+function sayHello() {
+    alert("hello there!");
+}
